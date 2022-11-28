d70ff7d80a6a333dfb92b5125475e4f2d8153660
sergey radionov <rsatom@gmail.com>
2022-11-28 10:38:52 +0700

    it's required to use proxy for build on snapcraft.io
    https://forum.snapcraft.io/t/no-internet-access-while-building-snap-on-snapcraft-io/32876

--- generate.sh
+++ generate.sh
@@ -1,11 +1,20 @@
 #!/bin/bash -e
 
+if [[ -z "${http_proxy}" ]]; then
+    PROXY=""
+else
+    PROXY=${http_proxy#"http://"}
+    PROXY=-r${PROXY%"/"}
+fi
+
+
 # -c++11  generate C++11 source code
 # -d      use DOM to populate xs:any, xs:anyType, and xs:anyAttribute
 # -p      create polymorphic types inherited from base xsd__anyType
 # -O4     optimize -O3 and omit unused schema root elements (use only with WSDLs)
 # -ofile  output to file
 wsdl2h -c++11 -d -p -O4 -o onvif.h \
+    ${PROXY} \
     www.onvif.org/ver10/device/wsdl/devicemgmt.wsdl \
     www.onvif.org/ver10/media/wsdl/media.wsdl \
     www.onvif.org/ver10/events/wsdl/event.wsdl
