021197ee4f4fbd978c4c4cd30010bf983a1e7033
sergey radionov <rsatom@gmail.com>
2026-06-24 19:19:32 +0700

    SOAP: always set network limits

--- SOAP.h
+++ SOAP.h
@@ -5,6 +5,7 @@
 struct SOAP {
     SOAP(soap_mode mode = SOAP_IO_DEFAULT) noexcept {
         soap_init1(&_soap, mode);
+        setLimits();
     }
     SOAP& operator = (SOAP&) = delete;
     ~SOAP() noexcept {
@@ -20,5 +21,20 @@ struct SOAP {
         return &_soap;
     }
 
+private:
+    enum {
+        SOAP_TIMEOUT = 3, // seconds
+        RECV_MAX_LENGTH = 1024 * 1024, // 1 megabyte
+    };
+
+    void setLimits() noexcept {
+        _soap.connect_timeout = SOAP_TIMEOUT;
+        _soap.send_timeout = SOAP_TIMEOUT;
+        _soap.recv_timeout = SOAP_TIMEOUT;
+        _soap.transfer_timeout = SOAP_TIMEOUT;
+        _soap.recv_maxlength = RECV_MAX_LENGTH;
+    }
+
+private:
     struct soap _soap;
 };
